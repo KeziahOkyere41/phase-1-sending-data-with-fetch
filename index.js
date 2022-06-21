e104f8771f389e2498009073ccd7cb678896405a
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-21 21:14:03 +0000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const submitData = {
+    userName: "Keziah Okyere",
+    userEmail: "keziah.okyere@student.moringaschool.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+        alert("Oops!!! an error occurred, sorry");
+        console.log(error.message);
+      });
